da07b9aa612a5f885a764a4634b191f3255b7170
Denis Zubkov <denzu@mac.com>
2020-02-01 19:13:03 +0300

    Initial Commit

---
+++ ARClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARClient
+//
+//  Created by Dennis Zubkoff on 01.02.2020.
+//  Copyright © 2020 Denis Zubkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
